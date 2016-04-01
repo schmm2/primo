7ac88cc5466d53bcf77378e13a5810d16e271f0f
schmm2 <martinschmidli@Martins-MacBook-Pro.local>
2016-04-01 13:17:06 +0200

    fixed social links

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,6 @@ $( document ).ready(function() {
 	$("#nav-switch").on('click',function(){		
 		$("#nav-switch").toggleClass("active");
 		$('html,body').toggleClass('disable_scroll');
-		$("#nav-mobile").toggleClass("active");	
+		$("#header").toggleClass("nav-mobile-active");	
 	});	
 });
