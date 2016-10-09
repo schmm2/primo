eb1b99aa1a88f5865ac4f213bd990452f6ce9abc
schmm2 <tinsu3@gmail.com>
2016-10-09 12:23:04 +0200

    add copy

--- assets/js/post.js
+++ assets/js/post.js
@@ -4,9 +4,35 @@ $( document ).ready(function() {
 		$("#post-comments").remove();
 	}
 	
+	var i = 0;
 	// highlight the code
-	$('code').each(function(i, e) {hljs.highlightBlock(e)});
+	$('code').each(function(i, e) {
+		hljs.highlightBlock(e) 
+		$(this).attr("id","code-"+i);
+		$(this).wrap("<div class='code_wrap'></div");
+		$(this).after("<div data-clipboard-target='#code-"+i+"'class='copy hover'></div>");
+		i++;
+	});
 	
+	var clipboard = new Clipboard('.copy');	
+	
+	clipboard.on('click', function(){
+		console.log($(this));
+	});
+	
+	clipboard.on('success', function(e) {
+		e.clearSelection();
+		
+		$(e.trigger).removeClass('hover')
+		$(e.trigger).addClass('copy_done');
+		
+		window.setTimeout(function() {
+	        $(e.trigger).removeClass('copy_done');
+	        $(e.trigger).addClass('hover');
+	    }, 2000);
+	  
+	});
+		
 	// video fluid size
 	$("#post-content").fitVids();
  
@@ -14,9 +40,9 @@ $( document ).ready(function() {
 	$("#post-content img").each(function(index){
 		$(this).wrap('<a class="lightbox" href="'+this.src+'"></a>');
 	});
-	$().fluidbox({
-       
-    });
+	
+	
+
     $('.lightbox')
 	.on('openstart.fluidbox', disableScroll)
 	.on('closestart.fluidbox', enableScroll)
